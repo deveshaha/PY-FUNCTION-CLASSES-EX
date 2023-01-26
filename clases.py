e1219fcd4dba16c939f507f02bd8e9b1acc86999
deveshaha <devesh_hanumante@outlook.com>
2023-01-26 12:14:55 +0100

    update: third exs

--- clases.py
+++ clases.py
@@ -9,6 +9,9 @@
 # La clase Poligono() tendrá un método llamado darlados( ) que le pedirá al usuario que 
 # introduzca uno a uno los valores de todos los lados. 
 
+# La clase Polígono() tendrá otro método llamado verlados( ) que mostrará uno a uno los 
+# valores introducidos de todos los lados. 
+
 class Poligono:
     def __init__(self, num_lados):
         if num_lados < 3:
@@ -20,8 +23,12 @@ class Poligono:
 
     def darLados(valLado, pol):
         for i in range(pol.num_lados):
-            valLado = int(input(f'Introduce el valor del lado {i+1}: ''))
+            valLado = int (input(f'Introduce el valor del lado {i+1}: '))
             pol.lados[i] = valLado
+
+    def verLados(pol):
+        for i in range(pol.num_lados):
+            print(f'El valor del lado {i+1} es {pol.lados[i]}')
         
     
 
@@ -30,10 +37,16 @@ pol = int(input("Introduce el número de lados del polígono: "))
 pol = Poligono(pol)
 print(pol.lados)
 
-print("Punto 2: \n")
+print("\nPunto 2: \n")
 valLado = 0
 Poligono.darLados(valLado, pol)
 print(pol.lados)
+
+print("\nPunto 3: \n")
+Poligono.verLados(pol)
+
+
+
 
 
 
