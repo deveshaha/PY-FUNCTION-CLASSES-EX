97064ab42ae185f5dc01947907f1f140c99da569
deveshaha <devesh_hanumante@outlook.com>
2023-01-26 12:16:41 +0100

    update: exs 4

--- clases.py
+++ clases.py
@@ -9,8 +9,15 @@
 # La clase Poligono() tendrá un método llamado darlados( ) que le pedirá al usuario que 
 # introduzca uno a uno los valores de todos los lados. 
 
+# Punto 3:
 # La clase Polígono() tendrá otro método llamado verlados( ) que mostrará uno a uno los 
 # valores introducidos de todos los lados. 
+
+# Punto 4:
+# Se va a crear una clase llamada Triangulo() que hereda de la clase  Poligono().  
+# Al crear un objeto triangulo, se invoca al constructor de la clase Poligono() al que se indica el 
+# número de lados = 3. 
+ 
 
 class Poligono:
     def __init__(self, num_lados):
@@ -29,6 +36,11 @@ class Poligono:
     def verLados(pol):
         for i in range(pol.num_lados):
             print(f'El valor del lado {i+1} es {pol.lados[i]}')
+
+
+class Triangulo(Poligono):
+    def __init__(self):
+        super().__init__(3)
         
     
 
@@ -44,6 +56,10 @@ print(pol.lados)
 
 print("\nPunto 3: \n")
 Poligono.verLados(pol)
+
+print("\nPunto 4: \n")
+tri = Triangulo()
+print(tri.lados)
 
 
 
