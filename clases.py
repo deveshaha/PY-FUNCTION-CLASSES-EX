e703f2a1a60840545591304c8f1db0b4891e9bd1
deveshaha <devesh_hanumante@outlook.com>
2023-01-26 12:11:12 +0100

    update: first exs

--- clases.py
+++ clases.py
@@ -1,30 +1,35 @@
-usr = int(input("Indica el numero de ejercicio que deseas ejecutar: (1 - 5) "))
+#Ejercicio de clases con herencia en Python
 
-match usr:
-    case 1:
-        ## Ejercicio 1
+# Punto 1:
+# Escribe una clase llamada Poligono() que genera objetos de polígono de 3 o más lados.  
+# Al crear un objeto, en el constructor __init__ ( ), se indica el número de lados que va a tener y 
+# se crea una lista que va a tener ese número de elementos cuyos valores iniciales serán 0. 
 
+# Punto 2:
+# La clase Poligono() tendrá un método llamado darlados( ) que le pedirá al usuario que 
+# introduzca uno a uno los valores de todos los lados. 
 
-        print("Ejercicio 1: \n")
-    case 2:
-        ## Ejercicio 2
-
-
-        print("Ejercicio 2: \n")
-
-    case 3:
-        ## Ejercicio 3
-        print("Ejercicio 3: \n")
-
-
-    case 4:
-        ##Ejercicio 4
-        print("Ejercicio 4: \n")
+class Poligono:
+    def __init__(self, num_lados):
+        if num_lados < 3:
+            print("Error: el polígono debe tener al menos 3 lados.")
+        else:
+            self.num_lados = num_lados
+            for i in range(num_lados):
+                self.lados = [0] * num_lados
 
 
-    case 5:
-        ##Ejercicio 5
-        print("Ejercicio 5: \n")
         
-    case _:
-        print("El número introducido no es válido")
+    
+
+print("Punto 1: \n")
+pol = int(input("Introduce el número de lados del polígono: "))
+pol = Poligono(pol)
+print(pol.lados)
+
+
+
+
+
+
+
